d5c4443dcc210aeaa301772db2ad48596048a729
techcare-dev <techcare-dev@Mac.Dlink>
2025-08-17 21:43:58 +0600

    Project Initialized with Dependencies

---
+++ App.js
@@ -0,0 +1,38 @@
+import {
+  StatusBar,
+  StyleSheet,
+  useColorScheme,
+  View,
+  Text,
+} from 'react-native';
+import { SafeAreaProvider, SafeAreaView } from 'react-native-safe-area-context';
+
+function App() {
+  const isDarkMode = useColorScheme() === 'dark';
+
+  return (
+    <SafeAreaProvider>
+      <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
+      <SafeAreaView style={styles.container}>
+        <AppContent />
+      </SafeAreaView>
+    </SafeAreaProvider>
+  );
+}
+
+function AppContent() {
+  return (
+    <View style={styles.container}>
+      <Text style={{ textAlign: 'left' }}>hello</Text>
+      <Text>dera akkkk</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+});
+
+export default App;
