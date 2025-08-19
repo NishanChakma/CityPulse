9a4f4338c6c38df2e6657cc4f68f1bd5d495631d
NishanChakma <nishancse13@gmail.com>
2025-08-20 00:12:14 +0600

    favourite item

--- src/components/EventList.js
+++ src/components/EventList.js
@@ -1,99 +1,8 @@
-import React, { useCallback } from 'react';
-import {
-  FlatList,
-  Image,
-  StyleSheet,
-  Text,
-  TouchableOpacity,
-  View,
-} from 'react-native';
-import { useNavigation } from '@react-navigation/native';
-import { useDispatch, useSelector } from 'react-redux';
-import dayjs from 'dayjs';
-
+import React from 'react';
+import { FlatList, StyleSheet, Text, View } from 'react-native';
+import { useSelector } from 'react-redux';
+import EventCard from './EventCard';
 import colors from '../utills/colors';
-import fav from '../assests/fav.png';
-import red from '../assests/red.png';
-import calendar from '../assests/calendar.png';
-import location from '../assests/location.png';
-import AppRoutes from '../navigation/AppRoutes';
-import { setCurrentEvent, setFavourite } from '../store/slices/eventSlice';
-
-// ✅ Single Event Card
-const RenderItem = React.memo(({ item }) => {
-  const dispatch = useDispatch();
-  const navigation = useNavigation();
-  const favorites = useSelector(state => state.event.favorites);
-
-  const handleFavourite = useCallback(() => {
-    const updatedFavorites = favorites.some(e => e.id === item.id)
-      ? favorites.filter(e => e.id !== item.id)
-      : [...favorites, item];
-
-    dispatch(setFavourite(updatedFavorites));
-  }, [dispatch, favorites, item]);
-
-  const handleDetails = useCallback(() => {
-    dispatch(setCurrentEvent(item));
-    navigation.navigate(AppRoutes.EVENTDETAILSSCREEN);
-  }, [dispatch, item]);
-
-  return (
-    <TouchableOpacity onPress={handleDetails}>
-      <View style={styles.card}>
-        {/* Event Image */}
-        {item?.images?.[0]?.url ? (
-          <Image
-            source={{ uri: item.images[0].url, cache: 'force-cache' }}
-            style={styles.image}
-          />
-        ) : (
-          <View style={[styles.image, styles.imagePlaceholder]} />
-        )}
-
-        {/* Event Details */}
-        <View style={styles.mid}>
-          <Text style={styles.event} numberOfLines={1}>
-            {item?.name || 'Untitled Event'}
-          </Text>
-
-          {/* Date */}
-          <View style={styles.flex}>
-            <Image source={calendar} style={styles.icon} />
-            <Text style={styles.date} numberOfLines={1}>
-              {item?.dates?.start?.dateTime
-                ? dayjs(item.dates.start.dateTime).format('ddd, D MMM YYYY')
-                : 'Date TBA'}
-            </Text>
-          </View>
-
-          {/* Location */}
-          <View style={styles.flex}>
-            <Image
-              source={location}
-              style={[styles.icon, { marginRight: 5 }]}
-            />
-            <Text style={styles.date} numberOfLines={1}>
-              {item?._embedded?.venues?.[0]?.address?.line1 ||
-                'Unknown Address'}
-              {item?._embedded?.venues?.[0]?.city?.name
-                ? `, ${item._embedded.venues[0].city.name}`
-                : ''}
-            </Text>
-          </View>
-        </View>
-
-        {/* Favourite Button */}
-        <TouchableOpacity style={styles.touch} onPress={handleFavourite}>
-          <Image
-            source={favorites.some(e => e.id === item.id) ? red : fav}
-            style={styles.favIcon}
-          />
-        </TouchableOpacity>
-      </View>
-    </TouchableOpacity>
-  );
-});
 
 // ✅ Event List
 const EventList = () => {
@@ -109,7 +18,7 @@ const EventList = () => {
       <FlatList
         data={events}
         keyExtractor={item => item.id}
-        renderItem={({ item }) => <RenderItem item={item} />}
+        renderItem={({ item }) => <EventCard item={item} />}
         scrollEnabled={false} // keep parent ScrollView scrolling
         showsVerticalScrollIndicator={false}
       />
@@ -139,63 +48,5 @@ const styles = StyleSheet.create({
     textAlign: 'center',
     paddingBottom: 20,
     paddingHorizontal: 40,
-  },
-  card: {
-    flexDirection: 'row',
-    alignItems: 'center',
-    backgroundColor: colors.inputColor,
-    borderRadius: 8,
-    padding: 10,
-    marginBottom: 12,
-  },
-  image: {
-    height: 64,
-    width: 64,
-    borderRadius: 6,
-    backgroundColor: '#eee',
-  },
-  imagePlaceholder: {
-    justifyContent: 'center',
-    alignItems: 'center',
-  },
-  mid: {
-    flex: 1,
-    marginLeft: 10,
-    marginRight: 40,
-  },
-  event: {
-    fontSize: 16,
-    fontWeight: '600',
-    color: colors.textPrimary,
-  },
-  date: {
-    fontSize: 12,
-    fontWeight: '400',
-    color: colors.textSecondary,
-    flexShrink: 1,
-  },
-  icon: {
-    height: 12,
-    width: 12,
-  },
-  flex: {
-    flexDirection: 'row',
-    alignItems: 'center',
-    marginTop: 4,
-  },
-  touch: {
-    position: 'absolute',
-    right: 10,
-    top: 10,
-  },
-  favIcon: {
-    height: 24,
-    width: 24,
-  },
-  notFound: {
-    fontSize: 14,
-    color: colors.primary,
-    textAlign: 'center',
-    paddingTop: 30,
   },
 });
--- src/screens/FavoriteScreen.js
+++ src/screens/FavoriteScreen.js
@@ -1,14 +1,35 @@
-import { StyleSheet, Text, View } from 'react-native';
+import { FlatList, StyleSheet, Text, View } from 'react-native';
 import React from 'react';
+import { useSelector } from 'react-redux';
+import EventCard from '../components/EventCard';
+import colors from '../utills/colors';
 
 const FavoriteScreen = () => {
+  const favorites = useSelector(state => state.event.favorites);
+
   return (
-    <View>
-      <Text>FavoriteScreen</Text>
+    <View style={{ flex: 1, paddingHorizontal: 10 }}>
+      <Text style={styles.fav}>Your Favourite Event's</Text>
+      <FlatList
+        data={favorites}
+        keyExtractor={item => item.id}
+        renderItem={({ item }) => <EventCard item={item} />}
+        scrollEnabled={false} // keep parent ScrollView scrolling
+        showsVerticalScrollIndicator={false}
+      />
     </View>
   );
 };
 
 export default FavoriteScreen;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  fav: {
+    fontSize: 20,
+    color: colors.textPrimary,
+    textAlign: 'center',
+    fontWeight: 700,
+    paddingBottom: 20,
+    paddingTop: 10,
+  },
+});
